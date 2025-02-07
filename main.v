718567b9c7a6332014c9c6fb1d71eae455110e23
Linklancien <linklancien@gmail.com>
2025-02-07 22:29:56 +0100

    Background & Window
    Added a window and a background from RAIN WORLD

--- main.v
+++ main.v
@@ -3,31 +3,63 @@ module main
 import os
 import time
 import miniaudio as ma
-import math as m
+import gg
+
+const bg_color = gg.Color{0, 0, 0, 0}
+
+struct App {
+	mut:
+	ctx		&gg.Context = unsafe { nil }
+
+	// Sons
+	engine	&ma.Engine
+	sounds	[]ma.Sound	=	[ma.Sound{}]
+
+	// Files
+	basedir string
+
+	// Graphics
+	background gg.Image
+}
 
 fn main() {
-	basedir := os.real_path(os.join_path(os.dir(@FILE)))
-	wav_file := os.join_path(basedir, 'audio.flac')
+	mut app := &App{engine: &ma.Engine{}}
+	app.ctx = gg.new_context(
+		fullscreen: false
+		width: 100*8
+		height: 100*8
+		create_window: true
+		window_title: '- 3D -'
+		user_data: app
+		bg_color: bg_color
+		init_fn:  on_init
+		frame_fn: on_frame
+		event_fn: on_event
+		sample_count: 4
+	)
 
-	engine := &ma.Engine{}
+	app.basedir = os.real_path(os.join_path(os.dir(@FILE)))
+	wav_file := os.join_path(app.basedir, 'audio.flac')
 
 	// mut engineconfig := ma.engine_config_init()
 	// engineconfig.noDevice   = ma.true
 	// engineconfig.channels   = 2        // Must be set when not using a device.
 	// engineconfig.sampleRate = 48000    // Must be set when not using a device.
 
-	result := ma.engine_init(ma.null, engine)
+	result := ma.engine_init(ma.null, app.engine)
 	if result != .success {
 		panic('Failed to initialize audio engine.')
 	}
-	ma.engine_listener_set_position(engine, 0, 0, 0, 0)
-	// ma.engine_listener_set_direction(engine, 0, 0, 0, 0)
-	ma.engine_listener_set_cone(engine, 0, 1, 3, 0.5)
+	ma.engine_listener_set_position(app.engine, 0, 0, 0, 0)
+	// ma.engine_listener_set_direction(app.gg.engine, 0, 0, 0, 0)
+	ma.engine_listener_set_cone(app.engine, 0, 1, 3, 0.5)
 
 	// Sound
-	sound := ma.Sound{}
+	app.sounds[0] = ma.Sound{}
 
-	if ma.sound_init_from_file(engine, wav_file.str, 0, ma.null, ma.null, &sound) != .success {
+	sound := &app.sounds[0]
+
+	if ma.sound_init_from_file(app.engine, wav_file.str, 0, ma.null, ma.null, &sound) != .success {
 		panic('Failed to load and play "${wav_file}".')
 	}
 	
@@ -38,18 +70,58 @@ fn main() {
 	ma.sound_set_position(sound, 0, 0, 3)
 
 	ma.sound_start(&sound)
-	mut t := 0.0
-	for ma.sound_at_end(&sound) != 1{
+	
+	// background
+	app.background = app.ctx.create_image(os.join_path(app.basedir, 'back.jpg'))!
+
+
+	app.ctx.run()
+	app.exit()
+}
+
+fn on_init(mut app App){
+	size := app.ctx.window_size()
+	app.ctx.width 		= size.width
+	app.ctx.height 		= size.height
+}
+
+fn on_frame(mut app App){
+	if ma.sound_at_end(&app.sounds[0]) != 1{
 		time.sleep(100 * time.millisecond)
 		// ma.sound_set_volume(&sound, 1)
 		// println(ma.sound_get_volume(&sound))
-		t += 0.05
 		// println("$t, ${2*m.sin(t)}, ${2*m.cos(t)} ")
-		ma.sound_set_position(sound, f32(2*m.sin(t)), 0, f32(2*m.cos(t)))
 	}
-	
 
+	app.ctx.begin()
+	app.ctx.draw_image(0, 0, app.ctx.width, app.ctx.height, app.background)
+	app.ctx.end()
+}
+
+fn on_event(e &gg.Event, mut app App){
+	size := app.ctx.window_size()
+	app.ctx.width 		= size.width
+	app.ctx.height 		= size.height
+
+	match e.typ {
+		.key_down {
+			match e.key_code{
+				.escape{
+					app.exit()
+				}
+				else{}
+			}
+		}
+		else{}
+	}
+}
+
+fn (app App) exit(){
 	// Déconnexion
-	ma.engine_uninit(engine)
-	ma.sound_uninit(sound)
+	ma.engine_uninit(app.engine)
+	for sound in app.sounds{
+		ma.sound_uninit(sound)
+	}
+	app.ctx.quit()
+	print("Quit")
 }
